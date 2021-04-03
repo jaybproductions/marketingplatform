f199849f94e9dd94daae8dd80c1c08305f059252
JayB Productions <jaybgameproductions@gmail.com>
2021-04-03 17:13:44 -0400

    adding temp dashboard notice

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,8 +2,20 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="dashboard">
+    <div
+      className="dashboard"
+      style={{
+        textAlign: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingTop: "100px",
+      }}
+    >
       <h1>Dashboard Page</h1>
+      <p>
+        This page is still a work in progress....Please feel free to vist other
+        pages.
+      </p>
     </div>
   );
 };

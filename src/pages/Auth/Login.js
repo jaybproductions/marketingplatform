2682ff67b13edca8a582090b73fe8c4259e367ce
jaybproductions <jaybgameproductions@gmail.com>
2021-05-26 16:34:22 -0400

    fixing login screen

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -3,8 +3,9 @@ import { Link, withRouter } from "react-router-dom";
 import validateLogin from "../../validators/validateLogin";
 import useForm from "../../hooks/useForm";
 import firebase from "../../firebase";
-import { Button, TextField } from "@material-ui/core";
+import { Button, TextField, Card, CardContent } from "@material-ui/core";
 import { toast, ToastContainer } from "react-toastify";
+import "../../css/Login.css";
 
 const INITIAL_STATE = {
   email: "",
@@ -37,45 +38,52 @@ const Login = (props) => {
 
   return (
     <div className="login" style={{ textAlign: "center" }}>
-      <h1 style={{ paddingTop: "10px", textAlign: "center" }}>
-        Marketing Platform Login
-      </h1>
-      <br />
-      <form lines="full">
-        <TextField
-          name="email"
-          variant="outlined"
-          type="text"
-          label="Email"
-          required
-          value={values.email}
-          onChange={handleChange}
-        />
-        <br />
-        <br />
-        <TextField
-          name="password"
-          label="Password"
-          type="password"
-          variant="outlined"
-          required
-          value={values.password}
-          onChange={handleChange}
-        />
-      </form>
-      <br />
-      <Button
-        variant="contained"
-        type="submit"
-        color="primary"
-        expand="block"
-        onClick={handleSubmit}
-        disabled={isSubmitting}
-      >
-        Log In
-      </Button>
-      <br />
-      <Link to={"/forgot"}>Forgot Password?</Link>
+      <Card>
+        <CardContent>
+          <h1 style={{ paddingTop: "10px", textAlign: "center" }}>
+            Marketing Platform Login
+          </h1>
+          <br />
+          <form lines="full">
+            <TextField
+              name="email"
+              variant="outlined"
+              type="text"
+              label="Email"
+              fullWidth
+              required
+              value={values.email}
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+            <TextField
+              name="password"
+              fullWidth
+              label="Password"
+              type="password"
+              variant="outlined"
+              required
+              value={values.password}
+              onChange={handleChange}
+            />
+          </form>
+          <br />
+          <Button
+            variant="contained"
+            type="submit"
+            color="primary"
+            expand="block"
+            onClick={handleSubmit}
+            disabled={isSubmitting}
+          >
+            Log In
+          </Button>
+          <br />
+          <Link to={"/forgot"}>Forgot Password?</Link>
+        </CardContent>
+      </Card>
+
       <ToastContainer />
     </div>
   );
